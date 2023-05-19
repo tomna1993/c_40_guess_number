44045414ddf3ca45b2a4edeb25c0df41e2f45786
TomasNagy <tomi.nagy@outlook.com>
2023-05-19 19:27:49 +0200

    Add comments

---
+++ guess_number.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+	// Read command-line argument; if program run with a
+	// single command-line argument, print error and return 1
+
+
+	// Convert argument from string to int, 
+	// If it is equal to 0, bigger then 30000 or not a number return 1
+
+	// Print range to inform the user 
+
+	// Generate a random number 
+
+	// Start the game
+
+	// Print "You win!" or "Game Over!"
+}
